9a4fa7897cb08ae32073041a13a39f4602534f77
hebiscus <107350293+hebiscus@users.noreply.github.com>
2022-12-22 14:20:03 +0100

    background divs and their styling, font

--- src/home.js
+++ src/home.js
@@ -1,12 +1,6 @@
 export default function Home() {
     const content = document.getElementById("content");
 
-    const divLayer = document.createElement("div");
-    divLayer.classList.add("divlayer");
-    content.appendChild(divLayer);
-    
-    const header = document.createElement("div");
-    header.classList.add("header");
-    divLayer.appendChild(header);
+  
     
   }
--- src/index.js
+++ src/index.js
@@ -5,3 +5,32 @@ import "./style.css";
 
 Home();
 
+(function () {
+    const content = document.getElementById("content");
+
+    const divLayer = document.createElement("div");
+    divLayer.classList.add("divlayer");
+    content.appendChild(divLayer);
+    
+    const header = document.createElement("div");
+    header.classList.add("header");
+    divLayer.appendChild(header);
+
+    const middlediv = document.createElement("div");
+    middlediv.classList.add("middlediv");
+    divLayer.appendChild(middlediv);
+
+    const footer = document.createElement("div")
+    footer.classList.add("footer");
+    divLayer.appendChild(footer);
+
+    const caramenotitle = document.createElement("p");
+    caramenotitle.innerText = "Carameno"
+    caramenotitle.classList.add("caramenotitle");
+    header.appendChild(caramenotitle);
+
+    const footertext = document.createElement("p");
+    footertext.innerText = "Copyright Ⓒ 2022 hebiscus";
+    footertext.classList.add("footertext");
+    footer.appendChild(footertext);
+})();
